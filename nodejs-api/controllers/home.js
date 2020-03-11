c6c0d95d683b0398c3916ed0c7eb5fe3be5df7a3
Gabriel Freire <gabrielfvale@gmail.com>
2020-03-11 16:51:18 -0300

    API: Controller para rota de versão

--- nodejs-api/controllers/home.js
+++ nodejs-api/controllers/home.js
@@ -1,14 +1,7 @@
-const { API_OLHO_VIVO_VERSAO } = process.env;
-
 module.exports = {
     getHomePage(_, res) {
         res.json({
             status: 'running'
-        });
-    },
-    getApiVersion(_, res) {
-        res.json({
-            sptransVersion: `v${API_OLHO_VIVO_VERSAO}`
         });
     }
 };
